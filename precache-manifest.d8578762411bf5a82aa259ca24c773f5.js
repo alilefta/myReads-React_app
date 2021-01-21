9a146ecade8f6d63906c3e7aa604613aaaa0948a
alilefta <alilefta95@gmail.com>
2021-01-21 12:04:58 +0300

    Initial Commit

---
+++ precache-manifest.d8578762411bf5a82aa259ca24c773f5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5a799ae69fa854cc6241",
+    "url": "/static/js/main.5a799ae6.chunk.js"
+  },
+  {
+    "revision": "5699e883a81065ac0778",
+    "url": "/static/js/1.5699e883.chunk.js"
+  },
+  {
+    "revision": "5a799ae69fa854cc6241",
+    "url": "/static/css/main.9928dc3f.chunk.css"
+  },
+  {
+    "revision": "4e7f6681e22a4ecf07fac5bfa150b3a5",
+    "url": "/index.html"
+  }
+];
